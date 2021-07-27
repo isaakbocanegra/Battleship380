f44b2bbbf78a0ec9001863bfe5617626960fb735
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2021-07-26 23:45:59 -0700

    testscript
    test comment

---
+++ Assets/Scenes/L.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hii
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
