835468e634502c709c2b7e10f8c10f990a85beb9
ChessbeatsCheckers <86694290+ChessbeatsCheckers@users.noreply.github.com>
2021-08-12 21:37:21 -0700

    Revert "Ship placement actions"
    This reverts commit 75e9a7a32522a01bc68ff9704c50944315aea041.

--- Assets/Scripts/shipplacements.cs
+++ Assets/Scripts/shipplacements.cs
@@ -2,91 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shipplacements : MonoBehaviour{
-    void Start(){
-        placeship data = new placeship();
-        data.placeships(1,3,"010203");
-        data.placeships(1,3,"011121");
-        data.placeships(1,2,"5152");
+public class shipplacements : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
 
-        data.printPlayersBoard(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
 
 
-public class placeship : MonoBehaviour{
+public class placeship
+    {
+    private int[,] plr1board = {
 
-    public int[,] plr1board = {  { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0} };
-    public int[,] plr2board = {  { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0} };
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0}
+        };
+    private int[,] p2r1board = {
 
-    public placeship(){
-      /*  plr1board= {{ 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0} };
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0},
+        { 0,0,0,0,0,0,0,0}
+        };
 
-        plr2board = {  { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0},
-        { 0,0,0,0,0,0,0,0},{ 0,0,0,0,0,0,0,0} };*/
-        
+    private  string direction;
+    private int shipsize; 
+    private int centerposition;
+
+
+    public placeship(string direct, int center, int shipsize)
+    { 
+
+
+    
+    
     }
 
-    public void placeships(int player, int shipsize, string RowColumn){
-        if(isCollision(player, shipsize, RowColumn)){
-            print(RowColumn +" is taken. Pick somewhere else!");
-        }
-        else{
-            // places ships on coords 
-            for (int i = 0; i <shipsize; i++){
-                int j =0, k=1;  // j & k are x&y coords
-                if(player==1){
-                    plr1board[j,k] = 1;
-                }
-                else if(player==2){
-                    plr2board[j,k] = 1;
-                }
-                j = j+2;
-                k = k+2;
-            }
-        }
-    }
 
-    // passing coords as string array "01"  "02", "03"
-    private bool isCollision(int player, int shipsize, string RowColumns){
-        int num = 0; // stores coord into array
-        for (int i = 0; i<shipsize; i++){
-            int j =0, k =1;
-            if (player==1){
-                if(plr1board[j,k] == 1){
-                    num++;
-                }
-            }
-            else{
-                if(plr2board[j,k] == 1){
-                    num++;
-                }
-            }
-        }
 
-        if(num == 0){
-            return false;
-        }
-        return true;
-    }
 
-    public void printPlayersBoard(int player){
-        if (player==1){
-            for(int i =0; i<8; i++){
-                //print("\n");
-                print(plr1board[i,0]+" "+plr1board[i,1]+" "+plr1board[i,2]+" "+plr1board[i,3]+" "+plr1board[i,4]+" "+plr1board[i,5]+" "+plr1board[i,6]+" "+plr1board[i,7]);
-            }
-        }
-    }
+    
+
 }
