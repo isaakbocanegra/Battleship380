42bb3553999b6b0e5b0e5134c66bc1d24603312d
Miroo Yum <74537765+mooshiyum@users.noreply.github.com>
2021-08-13 23:21:53 -0700

    Player2Board is interact-able with ships
    PROBLEM: p2board kinda inherits p1boards stuffs (like highlighting boxes)

--- Assets/Scripts/Player2Board.cs
+++ Assets/Scripts/Player2Board.cs
@@ -6,100 +6,52 @@ using UnityEngine.UI;
 public class Player2Board : MonoBehaviour
 {
     // Game Logic:
-    private const int GRID_COUNT_X = 8;
-    private const int GRID_COUNT_Y = 8;
+    public const int GRID_COUNT_X = 8;
+    public const int GRID_COUNT_Y = 8;
     private const int GRID_SPACE_SIZE = 1;
-    public GameObject[,] gridSpaces2;
-    private Camera currentCamera;
-    private Vector2Int currentHover;
+    public GameObject[,] gridSpaces1;
     public Sprite tempGridSpaces;
 
-    //Player 2 Board Parent creation
+    //Player 1 Board Parent Creation
     GameObject p2BoardParent;
     
-    // Player 2 ship Color and Sprite Assigner
+    // Player 1 ship Color and Sprite Assigner
     SpriteRenderer p2Ship_SpriteRenderer;
 
     private void Awake()
     {
         p2BoardParent = new GameObject();
-        p2BoardParent.name = "Player2Board";
+        p2BoardParent.name = "Player2BoardParent";
         GenerateShipGrid(GRID_SPACE_SIZE, GRID_COUNT_X, GRID_COUNT_Y);
         GameObject tempObj = GameObject.Find("Player2Board");
         Destroy(tempObj);
+        
+        //p1BoardParent.SetActive(false);
     }
 
     private void Update()
     {
-        Debug.Log(Input.mousePosition);
-        if(!currentCamera)
-        {
-            currentCamera = Camera.main;
-            return;
-        }
-
-        RaycastHit hitInfo;
-        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out hitInfo, 100, LayerMask.GetMask("Grid")))
-        {
-            // Get indexes of tile that mouse is on
-            Vector2Int hitPosition = LookupGridIndex(hitInfo.transform.gameObject);
-
-            // If we're hovering a tile after not hovering any tile, the following occurs:
-            if(currentHover == -Vector2Int.one)
-            {
-                currentHover = hitPosition;
-                gridSpaces2[hitPosition.x,hitPosition.y].layer = LayerMask.NameToLayer("Hover");
-            }
-
-            // If we're already hovering a tile, change last hovered, and move to next / none
-            if(currentHover != -Vector2Int.one)
-            {
-                gridSpaces2[currentHover.x,currentHover.y].layer = LayerMask.NameToLayer("Grid");
-                currentHover = hitPosition;
-                gridSpaces2[hitPosition.x,hitPosition.y].layer = LayerMask.NameToLayer("Hover");
-            }
-        }
-        else
-        {
-            if(currentHover != -Vector2Int.one)
-            {
-                gridSpaces2[currentHover.x,currentHover.y].layer = LayerMask.NameToLayer("Grid");
-                currentHover = -Vector2Int.one;
-            }
-        }
+        
     }
 
+    // Generates the Battleship Board
     private void GenerateShipGrid(float gridSpaceSize, int gridCountX, int gridCountY)
     {
-        gridSpaces2 = new GameObject[gridCountX, gridCountY];
+        gridSpaces1 = new GameObject[gridCountX, gridCountY];
         for(int x = 0; x < gridCountX; x++)
             for(int y = 0; y < gridCountY; y++)
-                gridSpaces2[x,y] = GenerateSingleGridSpace(gridSpaceSize, x, y);
+                gridSpaces1[x,y] = GenerateSingleGridSpace(gridSpaceSize, y, x); // swap these x and y -- optional for upright
     }
 
     private GameObject GenerateSingleGridSpace(float gridSpaceSize, int x, int y)
     {
-        GameObject gridSpaceObject = new GameObject(string.Format("X:{0}, Y{1}", x, y));
+        GameObject gridSpaceObject = new GameObject(string.Format("X:{0}, Y{1}", y, x));  //swap the names -- optional for upright
         gridSpaceObject.transform.parent = p2BoardParent.gameObject.transform;
-        gridSpaceObject.transform.position = new Vector2((float) (x+25)*1.33f, (float) y*1.33f);
+        gridSpaceObject.transform.position = new Vector2((float) (x-15)*1.33f, (float) (y-3)*-1.33f);
         gridSpaceObject.AddComponent<SpriteRenderer>().sprite = tempGridSpaces;
-        
-        gridSpaceObject.layer = LayerMask.NameToLayer("Grid");
-        gridSpaceObject.AddComponent<BoxCollider>();
-        gridSpaceObject.AddComponent<Button>();
+        gridSpaceObject.AddComponent<BoxCollider2D>();
+        gridSpaceObject.AddComponent<GridMouseActions>();
 
         return gridSpaceObject;
-    }
-
-    // Operations
-    private Vector2Int LookupGridIndex(GameObject hitInfo)
-    {
-        for(int x = 0; x < GRID_COUNT_X; x++)
-            for(int y = 0; y < GRID_COUNT_Y; y++)
-                if(gridSpaces2[x,y] == hitInfo)
-                    return new Vector2Int(x, y);
-
-        return -Vector2Int.one; // An invalid, should crash the game (hope it doesnt lol)
     }
 }
