ceb7a20979ef33f8aa89d9d05ff05c63f76959f3
isaakbocanegra <71801650+isaakbocanegra@users.noreply.github.com>
2021-08-16 13:38:35 -0700

    Minor text changes
    Just some minor text fixes in script names, etc

--- Assets/Scripts/shipplacements.cs
+++ Assets/Scripts/shipplacements.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shipplacements : MonoBehaviour
+public class ShipPlacements : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
